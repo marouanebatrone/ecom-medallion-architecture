30b92c2c2edd2b85e161413061cbb16dbd87d7be
marouane batrone <batrone.marouane@gmail.com>
2026-05-17 14:55:57 +0100

    Refactor MedallionPipeline to use environment variables for configuration and update OpenLineage package version

--- scripts/medallionPipeline/medallion_pipeline.py
+++ scripts/medallionPipeline/medallion_pipeline.py
@@ -2,36 +2,57 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import current_timestamp, col
 from pyspark.sql.types import FloatType, IntegerType, TimestampType
 import shutil
+import time
+import os
+from dotenv import load_dotenv
 
+load_dotenv("/opt/airflow/.env")
 
 class MedallionPipeline:
     
     def __init__(self):
-        
-        openlineage_package = "io.openlineage:openlineage-spark_2.12:1.13.1"
+        openlineage_package = "io.openlineage:openlineage-spark_2.12:1.43.0"
         postgres_package = "org.postgresql:postgresql:42.7.3"
-
+        
+        OPENLINEAGE_URL = os.getenv("OPENLINEAGE_URL")
+        OPENLINEAGE_ENDPOINT = os.getenv("OPENLINEAGE_ENDPOINT")
+        OPENLINEAGE_NAMESPACE = os.getenv("OPENLINEAGE_NAMESPACE")
+        
+        JDB_URL = os.getenv("JDB_URL")
+        JDB_USER = os.getenv("JDB_USER")
+        JDB_PASSWORD = os.getenv("JDB_PASSWORD")
+        JDB_DRIVER = os.getenv("JDB_DRIVER")
+        
+        INPUT_PATH = os.getenv("INPUT_PATH")
+        PROCESSED_PATH = os.getenv("PROCESSED_PATH")
+        
+        
         self.spark = SparkSession.builder \
             .appName("MedallionPipeline") \
             .config("spark.jars.packages", f"{postgres_package},{openlineage_package}") \
             .config("spark.extraListeners", "io.openlineage.spark.agent.OpenLineageSparkListener") \
             .config("spark.openlineage.transport.type", "http") \
-            .config("spark.openlineage.transport.url", "http://qupid-watchpipe.qupid.clusterdiali.me") \
-            .config("spark.openlineage.transport.endpoint", "/api/v1/lineage") \
-            .config("spark.openlineage.namespace", "spark-medallion-pipeline") \
+            .config("spark.openlineage.transport.url", OPENLINEAGE_URL) \
+            .config("spark.openlineage.transport.endpoint", OPENLINEAGE_ENDPOINT) \
+            .config("spark.openlineage.namespace", OPENLINEAGE_NAMESPACE) \
+            .config("spark.openlineage.facets.spark.logicalPlan.disabled", "false") \
+            .config("spark.openlineage.facets.debug.disabled", "false") \
+            .config("spark.openlineage.columnLineage.datasetLineageEnabled", "true") \
+            .config("spark.openlineage.capturedProperties", "spark.master,spark.app.name,spark.sql.shuffle.partitions,spark.executor.memory,spark.executor.cores,spark.driver.memory") \
             .getOrCreate()
 
         self.spark.sparkContext.setLogLevel("ERROR")
 
-        self.jdbc_url = "jdbc:postgresql://host.docker.internal:5433/sales_data_platform"
+        self.jdbc_url = JDB_URL
+
         self.properties = {
-            "user": "postgres",
-            "password": "admin",
-            "driver": "org.postgresql.Driver"
+            "user": JDB_USER,
+            "password": JDB_PASSWORD,
+            "driver": JDB_DRIVER
         }
 
-        self.input_path     = "/opt/airflow/data/input/unprocessed/"
-        self.processed_path = "/opt/airflow/data/input/processed/"
+        self.input_path     = INPUT_PATH
+        self.processed_path = PROCESSED_PATH
 
         self.files = {
             "customers":   "olist_customers_dataset.csv",
@@ -41,16 +62,12 @@ class MedallionPipeline:
             "sellers":     "olist_sellers_dataset.csv",
         }
 
-    # CSV → OLTP
     def ingest_to_oltp(self):
-        
-        # CUSTOMERS
         df = self.spark.read.option("header", True).csv(self.input_path + self.files["customers"]).withColumn("date_ingested", current_timestamp())
         df.write.jdbc(self.jdbc_url, "oltp.customers", "append", self.properties)
         shutil.move(self.input_path + self.files["customers"], self.processed_path + self.files["customers"])
         print("customers ingested to OLTP")
 
-        # ORDER ITEMS
         df = self.spark.read.option("header", True).csv(self.input_path + self.files["order_items"])
         df = df.withColumn("order_item_id", col("order_item_id").cast(IntegerType())) \
                .withColumn("shipping_limit_date", col("shipping_limit_date").cast(TimestampType())) \
@@ -61,7 +78,6 @@ class MedallionPipeline:
         shutil.move(self.input_path + self.files["order_items"], self.processed_path + self.files["order_items"])
         print("order_items ingested to OLTP")
 
-        # ORDERS
         df = self.spark.read.option("header", True).csv(self.input_path + self.files["orders"])
         for c in ["order_purchase_timestamp", "order_approved_at", 
                   "order_delivered_carrier_date", 
@@ -73,7 +89,6 @@ class MedallionPipeline:
         shutil.move(self.input_path + self.files["orders"], self.processed_path + self.files["orders"])
         print("orders ingested to OLTP")
 
-        # PRODUCTS
         df = self.spark.read.option("header", True).csv(self.input_path + self.files["products"])
         df = df.withColumn("product_name_lenght", col("product_name_lenght").cast(IntegerType())) \
                .withColumn("product_description_lenght", col("product_description_lenght").cast(IntegerType())) \
@@ -87,15 +102,12 @@ class MedallionPipeline:
         shutil.move(self.input_path + self.files["products"], self.processed_path + self.files["products"])
         print("products ingested to OLTP")
 
-        # SELLERS
         df = self.spark.read.option("header", True).csv(self.input_path + self.files["sellers"]).withColumn("date_ingested", current_timestamp())
         df.write.jdbc(self.jdbc_url, "oltp.sellers", "append", self.properties)
         shutil.move(self.input_path + self.files["sellers"], self.processed_path + self.files["sellers"])
         print("sellers ingested to OLTP")
         
-        self.spark.stop()
 
-    # OLTP → BRONZE
     def oltp_to_bronze(self):
         for table in self.files.keys():
             df = self.spark.read.jdbc(
@@ -106,12 +118,8 @@ class MedallionPipeline:
             df.write.jdbc(self.jdbc_url, "bronze." + table, "append", self.properties)
             print(f"{table} loaded to Bronze")
             
-        self.spark.stop()
 
-    # BRONZE → SILVER
     def bronze_to_silver(self):
-
-        # CUSTOMERS
         customers = self.spark.read.jdbc(
             self.jdbc_url,
             "(SELECT * FROM bronze.customers WHERE date_ingested::date = CURRENT_DATE) as t",
@@ -127,7 +135,6 @@ class MedallionPipeline:
          .write.jdbc(self.jdbc_url, "silver.customers", "append", self.properties)
         print("customers → Silver")
 
-        # ORDERS
         orders = self.spark.read.jdbc(
             self.jdbc_url,
             "(SELECT * FROM bronze.orders WHERE date_ingested::date = CURRENT_DATE) as t",
@@ -141,7 +148,6 @@ class MedallionPipeline:
         ).write.jdbc(self.jdbc_url, "silver.orders", "append", self.properties)
         print("orders → Silver")
 
-        # ORDER ITEMS
         order_items = self.spark.read.jdbc(
             self.jdbc_url,
             "(SELECT * FROM bronze.order_items WHERE date_ingested::date = CURRENT_DATE) as t",
@@ -157,7 +163,6 @@ class MedallionPipeline:
         ).write.jdbc(self.jdbc_url, "silver.order_items", "append", self.properties)
         print("order_items → Silver")
 
-        # PRODUCTS
         products = self.spark.read.jdbc(
             self.jdbc_url,
             "(SELECT * FROM bronze.products WHERE date_ingested::date = CURRENT_DATE) as t",
@@ -175,7 +180,6 @@ class MedallionPipeline:
          .write.jdbc(self.jdbc_url, "silver.products", "append", self.properties)
         print("products → Silver")
 
-        # SELLERS
         sellers = self.spark.read.jdbc(
             self.jdbc_url,
             "(SELECT * FROM bronze.sellers WHERE date_ingested::date = CURRENT_DATE) as t",
@@ -193,13 +197,8 @@ class MedallionPipeline:
 
         print("Bronze → Silver completed")
         
-        
-        self.spark.stop()
-
-    # SILVER → GOLD
 
     def silver_to_gold(self):
-
         orders = self.spark.read.jdbc(
             self.jdbc_url,
             "(SELECT * FROM silver.orders WHERE date_ingested::date = CURRENT_DATE) as t",
@@ -226,7 +225,6 @@ class MedallionPipeline:
             properties=self.properties
         )
 
-        # FACT TABLE
         fact_sales = orders.join(order_items, "order_id").select(
             col("order_id"),
             col("customer_id"),
@@ -251,4 +249,6 @@ class MedallionPipeline:
 
         print("Silver → Gold completed")
         
+        print("Waiting a moment for lineage events to flush...")
+        time.sleep(2)
         self.spark.stop()
